5fc2cafdb48e04f36c74a96d78a0ade3c108415e
AgibalauD <d.a.agibalau@gmail.com>
2026-09-10 22:18:27 +0300

    modified task3 lesson6

--- task3.ts
+++ task3.ts
@@ -1,13 +1,20 @@
 // Напишите метод analyze, который возвращает объект анализа пользователей
-
+interface User {
+  id: number;
+  name: string;
+  age: number;
+  isActive: boolean;
+}
+const USERS = [
+  { id: 1, name: "Анна", age: 24, isActive: true },
+  { id: 2, name: "Иван", age: 17, isActive: false },
+  { id: 3, name: "Мария", age: 31, isActive: true },
+  { id: 4, name: "Пётр", age: 19, isActive: false },
+  { id: 5, name: "Олег", age: 42, isActive: true },
+  { id: 6, name: "Даниил", age: 27, isActive: true },
+] satisfies User[];
 const userAnalyzer = {
-  users: [
-    { id: 1, name: "Анна", age: 24, isActive: true },
-    { id: 2, name: "Иван", age: 17, isActive: false },
-    { id: 3, name: "Мария", age: 31, isActive: true },
-    { id: 4, name: "Пётр", age: 19, isActive: false },
-    { id: 5, name: "Олег", age: 42, isActive: true },
-  ],
+  users: USERS,
 
   analyze() {
     const result = {
